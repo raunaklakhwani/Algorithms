239f81c8e8829a8d929d5421b17ff11c0cf1b191
raunaklakhwani <raunaklakhwani@gmail.com>
2014-11-25 15:41:32 -0800

    Added smart method with nlogn complexity

--- PairSumEqualToK/Main.py
+++ PairSumEqualToK/Main.py
@@ -3,7 +3,7 @@ import math
 import copy
 
 input = [1, 2, -1, 3, 6, 7, 9, 8]
-K = 20
+K = 10
 
 def bruteForceMethod() :
     '''
@@ -16,5 +16,24 @@ def bruteForceMethod() :
                 return [indexElement,input[j]]
     return "No Such Element Found"
 
+def smartMethod() :
+    '''
+    Time complexity is O(nlogn) due to sorting of elements
+    '''
+    input1 = copy.deepcopy(input)
+    input1.sort()
+    length = len(input)
+    start = 0
+    end = length - 1
+    while start < end : 
+        if input1[start] + input1[end] == K :
+            return [input1[start],input1[end]]
+        elif input1[start] + input1[end] < K : 
+            start = start + 1
+        else :
+            end = end - 1
+    return "No Such Element Found"     
+
 if __name__ == '__main__':
     print bruteForceMethod()
+    print smartMethod()
