4916421c325bd1074c051d82583f43a4572a71e3
raunaklakhwani <raunaklakhwani@gmail.com>
2014-11-25 15:34:21 -0800

    Added bruteForceMethod

--- PairSumEqualToK/Main.py
+++ PairSumEqualToK/Main.py
@@ -1,2 +1,20 @@
+import sys
+import math
+import copy
+
+input = [1, 2, -1, 3, 6, 7, 9, 8]
+K = 20
+
+def bruteForceMethod() :
+    '''
+    Time Complexity is O(n^2)
+    '''
+    length = len(input)
+    for i,indexElement in enumerate(input) : 
+        for j in range(i + 1, length) :
+            if indexElement + input[j] == K : 
+                return [indexElement,input[j]]
+    return "No Such Element Found"
+
 if __name__ == '__main__':
-    print 'hello'
+    print bruteForceMethod()
