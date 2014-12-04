d9e60a5d3a45e6b033b018ddf4cdf6a87d1639fc
raunaklakhwani <raunaklakhwani@gmail.com>
2014-12-03 20:06:42 -0800

    Added updated code for kthSmallestNumber

--- kthSmapllestElement/Main.py
+++ kthSmapllestElement/Main.py
@@ -1,10 +1,9 @@
 import random
-input = [21,3,2,4,5,6,7,1,8,9,50]
-
-def swap(indexA,indexB):
-    input[indexA],input[indexB] = input[indexB],input[indexA]
+input = [21, 3, 2, 4, 5, 6, 7, 1, 8, 9, 50]
+def swap(indexA, indexB):
+    input[indexA], input[indexB] = input[indexB], input[indexA]
     
-def partition(pivotIndex,start,end):
+def partition(pivotIndex, start, end):
     if pivotIndex != start :
         swap(pivotIndex, start)
     
@@ -15,18 +14,41 @@ def partition(pivotIndex,start,end):
             swap(i, j)
             i = i + 1
     
-    swap(i-1,start)
+    swap(i - 1, start)
     return i - 1
 
-def quickSort(start,end):
+def quickSort(start, end):
     if start < end :
-        randomPivot = random.randint(start,end)
+        randomPivot = random.randint(start, end)
         p = partition(randomPivot, start, end)
-        quickSort(start, p-1)
-        quickSort(p+1, end)
+        quickSort(start, p - 1)
+        quickSort(p + 1, end)
         
+def kthSmallestNumber(start, end, order):
+    if start <= end :
+        #print input
+        randomPivot = random.randint(start, end)
+        #print 'start = ',start,' end = ',end,' order = ',order
+        #print 'randomPivot = ',randomPivot
+        #print 'Element = ',input[randomPivot]
+        p = partition(randomPivot, start, end)
+        #print 'Updated Input = ',input
+        #print 'return = ',p
+        if p > start + order :
+            return kthSmallestNumber(start, p - 1, order)
+        elif p < start + order :
+            #order + start - p - 1 really important
+            return kthSmallestNumber(p + 1, end, order + start - p - 1)
+        else :
+            return input[p]
+
 
 if __name__ == '__main__':
-    print input
+    #print input
+    order = 5
+    value = kthSmallestNumber(0, len(input) - 1, order - 1)
+    print value
+    #print 'rr'
     quickSort(0, len(input) - 1)
     print input
+    print input[order - 1] == value
