9cf2da7136cbe64b1e921df6b6ff23c4f6fa2619
raunaklakhwani <raunaklakhwani@gmail.com>
2014-12-03 17:05:39 -0800

    Added quick sort code

--- kthSmapllestElement/Main.py
+++ kthSmapllestElement/Main.py
@@ -1,4 +1,32 @@
-input = [3,2,4,5,6,7,1,8]
+import random
+input = [21,3,2,4,5,6,7,1,8,9,50]
+
+def swap(indexA,indexB):
+    input[indexA],input[indexB] = input[indexB],input[indexA]
+    
+def partition(pivotIndex,start,end):
+    if pivotIndex != start :
+        swap(pivotIndex, start)
+    
+    pivot = input[start]
+    i = start + 1
+    for j in range(start + 1, end + 1) : 
+        if input[j] < pivot :
+            swap(i, j)
+            i = i + 1
+    
+    swap(i-1,start)
+    return i - 1
+
+def quickSort(start,end):
+    if start < end :
+        randomPivot = random.randint(start,end)
+        p = partition(randomPivot, start, end)
+        quickSort(start, p-1)
+        quickSort(p+1, end)
+        
 
 if __name__ == '__main__':
+    print input
+    quickSort(0, len(input) - 1)
     print input
